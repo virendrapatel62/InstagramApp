1d8b99c5a9c69949b68af50c3e5a70ad13b0f8f3
virendrapatel62 <patelvirendra62@gmail.com>
2025-05-01 14:40:52 +0530

    Added placeholder

--- app/src/screens/Home/Home.screen.tsx
+++ app/src/screens/Home/Home.screen.tsx
@@ -2,11 +2,18 @@ import { View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import HomeHeader from '../../components/organisms/HomeHeader/HomeHeader.component';
 import ScreenWrapper from '../../components/layouts/ScreenWrapper/ScreenWrapper.layout';
+import { Text } from '../../components/atoms';
 
 export default function HomeScreen() {
   return (
     <ScreenWrapper>
       <HomeHeader />
+
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text size="xxl" weight="bold">
+          Home Screen
+        </Text>
+      </View>
     </ScreenWrapper>
   );
 }
